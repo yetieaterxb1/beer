78f4d03aba7efaed250e0ae917c2df2bffb7d881
Grant Steinke <grantjsteinke@gmail.com>
2019-08-13 14:27:01 -0600

    brewery select and styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,23 +7,26 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+      renderBeers:[],
       beers: [],
       breweries: [],
       categories:[],
-      styles:[]
+      styles:[],
+      breweryid: 400
 		};
 
 		this.handleInputChange = this.handleInputChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBreweryChange = this.handleBreweryChange.bind(this);
   }
-  componentDidMount() {
+  componentWillMount() {
     Promise.all([
       fetch('/api/beers'),
       fetch('/api/breweries'),
       fetch('/api/categories'),
       fetch('/api/styles')
     ]).then(([res1,res2,res3,res4]) => Promise.all([res1.json(), res2.json(), res3.json(), res4.json()])).then(([data1,data2,data3,data4]) => this.setState({
-      beers: data1, breweries: data2, categories: data3, styles: data4
+      renderBeers: data1, beers: data1.filter(beer => beer.brewery_id === this.state.breweryid), breweries: data2, categories: data3, styles: data4
     }));
   }
 
@@ -45,31 +48,52 @@ class App extends React.Component {
 
 		this.props.handleSubmitButtonClick(formData);
 		e.preventDefault();
-	}
+  }
 
+  handleBreweryChange(event) {
+    console.log(typeof event.target.value)
+    this.setState({
+      breweryid: parseInt(event.target.value),
+      beers: this.state.renderBeers.filter(beer => beer.brewery_id === parseInt(event.target.value))
+    })
+  }
 	render() {
-    const beers = this.state.beers.slice(0,100)
+    console.log(this.state.breweries[this.state.breweryid])
+    console.log(this.state.beers)
+    console.log(this.state.breweries[0])
+    const beers = this.state.beers
     const breweries = this.state.breweries
     const categories = this.state.categories
     const styles = this.state.styles
-
     const userBeers = beers.filter(beer => beer.rating !== 0)
-    console.log(userBeers)
 		return (
       <>
       <section>
       <div class="text-effect">
+        <h1 class="neon" data-text="BEER FLIGHT" contenteditable>BEER FLIGHT</h1>
+        <div class="gradient"></div>
+        <div class="spotlight"></div>
 
-  <h1 class="neon" data-text="BEER FLIGHT" contenteditable>BEER FLIGHT</h1>
-  <div class="gradient"></div>
-  <div class="spotlight"></div>
-</div>
+      </div>
 			<br></br>
       <br></br>
-      <br></br>
-      <div>
-        <Modal class='modal' breweries={breweries} categories={categories} styles={styles}/>
+      <div class='modalbrewery'>
+        <div class='modalselect'>
+          <Modal class='modal' breweries={breweries} categories={categories} styles={styles}/>
+        </div>
+        <div class="breweryselect">
+          <div class="select">
+            <select  name="slct" id="slct" onChange={this.handleBreweryChange}  >
+              <option value="" disabled selected>Select Brewery</option>
+              {this.state.breweries.map(brewery => <option value={brewery.id}>{brewery.name}</option>)}
+            </select>
+          </div>
+        </div>
       </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <div class="tbl-header">
       <table class = "container" cellpadding="0" cellspacing="0" border="0">
         <tr>
@@ -87,3 +111,5 @@ class App extends React.Component {
 }
 
 export default App;
+
+
